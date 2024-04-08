ced31e70d6068ed50bdb247235e278020ac425f3
Yash Garg <yash.yg.garg111@gmail.com>
2024-04-08 14:03:03 +0530

    Updates

--- examserver/src/main/java/com/exam/ExamServerApplication.java
+++ examserver/src/main/java/com/exam/ExamServerApplication.java
@@ -4,31 +4,20 @@ import com.exam.helper.UserFoundException;
 import com.exam.model.Role;
 import com.exam.model.User;
 import com.exam.model.UserRole;
-import com.exam.model.exam.Quiz;
 import com.exam.repo.QuizRepository;
 import com.exam.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 //import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @SpringBootApplication
-public class ExamserverApplication implements CommandLineRunner {
+public class ExamServerApplication implements CommandLineRunner {
 
     @Autowired
     private UserService userService;
@@ -40,11 +29,7 @@ public class ExamserverApplication implements CommandLineRunner {
     public QuizRepository quizRepository;
 
     public static void main(String[] args) {
-
-
-        SpringApplication.run(ExamserverApplication.class, args);
-
-
+        SpringApplication.run(ExamServerApplication.class, args);
     }
 
     @Override
